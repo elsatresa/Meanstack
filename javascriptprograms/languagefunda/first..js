b99708e03d26f4c02938054073289305d252d413
Aleetamaria <75064477+Aleetamaria@users.noreply.github.com>
2020-12-02 12:14:54 +0530

    first java done

---
+++ javascriptprograms/languagefunda/first..js
@@ -0,0 +1,9 @@
+var num=12345;
+function add(){
+    console.log("test function")
+}
+class person{
+    setVal(){
+        
+    }
+}
---
+++ javascriptprograms/languagefunda/variable.js
@@ -0,0 +1,9 @@
+//variables
+//memory is used to represent a memory location
+//var variablename=value;
+//variable name is used represent a memory location
+//output function[console.log()]
+var anme="ajay";
+//welcome ajay
+console.log("welcome"+name);
+
